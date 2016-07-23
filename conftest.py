c5ce784329e58cf4381a28db36428c4692d18757
Nikita Kniazev <nok.raven@gmail.com>
2016-07-24 01:18:48 +0300

    Implemented test filtering for pytest

--- conftest.py
+++ conftest.py
@@ -2,19 +2,58 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import inspect
+import os
 import pytest
 import unittest
 
 import matplotlib
 matplotlib.use('agg')
 
-from matplotlib.testing.decorators import ImageComparisonTest
 from matplotlib import default_test_modules
+from matplotlib.testing.decorators import ImageComparisonTest
+
+
+IGNORED_TESTS = {
+    'matplotlib': [
+        'test_sankey',
+        'test_skew',
+        'test_ttconv',
+        'test_usetex',
+    ],
+}
+
+
+def blacklist_check(path):
+    head, tests_dir = os.path.split(path.dirname)
+    if tests_dir != 'tests':
+        return True
+    head, top_module = os.path.split(head)
+    return path.purebasename in IGNORED_TESTS.get(top_module, [])
+
+
+def whitelist_check(path):
+    head, tests_dir = os.path.split(path.dirname)
+    head, top_module = os.path.split(head)
+    test_name = '.'.join([top_module, tests_dir, path.purebasename])
+    return test_name not in default_test_modules
+
+
+COLLECT_FILTERS = {
+    'none': lambda _: False,
+    'blacklist': blacklist_check,
+    'whitelist': whitelist_check,
+}
 
 
 def is_nose_class(cls):
     return any(name in ['setUp', 'tearDown']
                for name, _ in inspect.getmembers(cls))
+
+
+def pytest_addoption(parser):
+    parser.addoption('--collect-filter', action='store',
+                     choices=COLLECT_FILTERS, default='whitelist',
+                     help='filter tests during collection phase')
 
 
 def pytest_configure(config):
@@ -23,6 +62,12 @@ def pytest_configure(config):
 
 def pytest_unconfigure(config):
     matplotlib._called_from_pytest = False
+
+
+def pytest_ignore_collect(path, config):
+    if path.ext == '.py':
+        collect_filter = config.getoption('--collect-filter')
+        return COLLECT_FILTERS[collect_filter](path)
 
 
 def pytest_pycollect_makeitem(collector, name, obj):
